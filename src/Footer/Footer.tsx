ba36bf56784af8389549783e46c967339737bbcd
Kevin Kobayashi <kkobayashi156@gmail.com>
2019-10-23 21:54:12 -0600

    feat: support for SASS files * portrait.png is now a square / reduced file size * moved heading logic out of Footer component * fixed display of footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import Portrait from './../assets/portrait.png';
-import linkedin from './../assets/linkedinIcon.jpg'
-import github from './../assets/githubIcon.jpg'
-import './Footer.css'
+import Linkedin from './../assets/linkedinIcon.jpg'
+import Github from './../assets/githubIcon.jpg'
+import './Footer.scss'
 
 
 const Footer: React.FC = () => {
   return (
     <div className="footer">
-      <h1>Cassandra Olfert</h1>
-      <h2>Second Year Computer Information Technology Student</h2>
-      <img src={Portrait} className="portrait" alt="Cassandra" />
       <a href="https://www.linkedin.com/in/cassandra-olfert-9b5745177" target="blank" rel="noopener noreferrer">
-        <img src={linkedin} className="icon" alt="LinkedIn Icon"></img>
+        <img src={Linkedin} className="icon" alt="LinkedIn Icon"></img>
       </a>
       <a href="https://github.com/olfertc" target="blank" rel="noopener noreferrer">
-        <img src={github} className="icon" alt="Github Icon"></img>
+        <img src={Github} className="icon" alt="Github Icon"></img>
       </a>
     </div>
   );
