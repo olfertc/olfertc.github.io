b5c6a49b247431202b028a45443accb624136a79
Cassandra Olfert <35616745+olfertc@users.noreply.github.com>
2019-10-23 20:04:05 -0600

    react refactor

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer/Footer'
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="profile">
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
